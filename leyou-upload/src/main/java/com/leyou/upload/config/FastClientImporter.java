413d52469a561cb307f9f18f58491ee02214daf7
yuyang <447395536@qq.com>
2019-10-28 21:21:17 +0800

    引入 FastDFS,分布式存储图片. 传至 CentOS6.8 测试没问题, 但本机访问 nginx 提示 404 , 不知为啥. 处理中. 备份

--- leyou-upload/src/main/java/com/leyou/upload/config/FastClientImporter.java
+++ leyou-upload/src/main/java/com/leyou/upload/config/FastClientImporter.java
@@ -1,5 +1,6 @@
 package com.leyou.upload.config;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
