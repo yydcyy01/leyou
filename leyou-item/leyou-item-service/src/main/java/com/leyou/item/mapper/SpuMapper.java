a7f16d15fb0df093b59fb6b1b0e1326d1788b0d6
yuyang <447395536@qq.com>
2019-10-29 16:42:38 +0800

    规格参数查询 实现

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/SpuMapper.java
@@ -0,0 +1,10 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Spu;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * 商品查询接口
+ */
+public interface SpuMapper extends Mapper<Spu> {
+}
