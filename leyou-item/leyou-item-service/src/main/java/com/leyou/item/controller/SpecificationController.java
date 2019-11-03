f7e1a31d595ea8c39e84ba79795e877ede1d35cb
yuyang <447395536@qq.com>
2019-11-03 09:33:19 +0800

    商品类别查询实现

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/SpecificationController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/SpecificationController.java
@@ -48,6 +48,7 @@ public class SpecificationController {
     /**
      * 原来的根据 gid（规格组id)查询规格参数的接口上，添加一个参数：cid，即商品分类id
      * 等一下， 考虑到以后可能还会根据是否搜索、是否为通用属性等条件过滤，我们多添加几个过滤条件：
+     * 即使参数为 null 会自动跳过
      * @param gid
      * @param cid
      * @param generic
