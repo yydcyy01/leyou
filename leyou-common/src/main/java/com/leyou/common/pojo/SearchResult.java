a4d43928af82ddaba14420740beac6a95b543560
yuyang <447395536@qq.com>
2019-11-06 22:52:33 +0800

    实现后特

---
+++ leyou-common/src/main/java/com/leyou/common/pojo/SearchResult.java
@@ -0,0 +1,46 @@
+package com.leyou.common.pojo;
+
+import java.util.List;
+import java.util.Map;
+
+public class SearchResult extends PageResult<Goods> {
+    
+    private List<Map<String, Object>> categories;
+    private List<Brand> brands;
+
+    public SearchResult() {
+    }
+
+    public SearchResult(List<Map<String, Object>> categories, List<Brand> brands) {
+        this.categories = categories;
+        this.brands = brands;
+    }
+
+    public SearchResult(List<Goods> items, Long total, List<Map<String, Object>> categories, List<Brand> brands) {
+        super(items, total);
+        this.categories = categories;
+        this.brands = brands;
+    }
+
+    public SearchResult(List<Goods> items, Long total, Integer totalPage, List<Map<String, Object>> categories, List<Brand> brands) {
+        super(items, total, totalPage);
+        this.categories = categories;
+        this.brands = brands;
+    }
+
+    public List<Map<String, Object>> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Map<String, Object>> categories) {
+        this.categories = categories;
+    }
+
+    public List<Brand> getBrands() {
+        return brands;
+    }
+
+    public void setBrands(List<Brand> brands) {
+        this.brands = brands;
+    }
+}
