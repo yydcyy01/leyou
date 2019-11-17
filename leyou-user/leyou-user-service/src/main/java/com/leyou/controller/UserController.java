b8d6f2cf65ba60a0aa0c5299ec94d21de920e66f
yuyang <447395536@qq.com>
2019-11-17 15:22:28 +0800

    实现 leyou-user 功能

--- leyou-user/leyou-user-service/src/main/java/com/leyou/controller/UserController.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/controller/UserController.java
@@ -2,10 +2,13 @@ package com.leyou.controller;
 
 import com.leyou.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -24,5 +27,20 @@ public class UserController {
 
         //200
         return ResponseEntity.ok(bool);
+    }
+
+    /**
+     * 发送短信验证码
+     * 文档中说 POST 方式, 传 String. => 是 @RequestParam(" ")
+     * @param phone
+     * @return
+     */
+    @PostMapping("code")
+    public ResponseEntity<Void> sendVerifyCode(@RequestParam("phone") String phone){
+        Boolean result=this.userService.sendVerifyCode(phone);
+        if(result==null||!result){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);//202，请求被接受
     }
 }
--- leyou-user/leyou-user-service/src/main/java/com/leyou/service/UserService.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/service/UserService.java
@@ -1,15 +1,36 @@
 package com.leyou.service;
 
+import com.leyou.common.utils.NumberUtils;
 import com.leyou.mapper.UserMapper;
 import com.leyou.user.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate ;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    static final String CODE_PRIFIX = "leyou_user";
+    //static Logger logger = new LoggerFactory.get...(UserService.class);
+    //factory 不是 new 的, 是静态类, 直接引用
+    static Logger logger= LoggerFactory.getLogger(UserService.class);
 
     /**
      * data, 要校验的数据，type要校验的类型 1、用户名 2、手机
@@ -31,9 +52,23 @@ public class UserService {
         return userMapper.selectCount(user) == 0;
     }
 
-    public Boolean sendVerifyCode(String phone){
-        // 生成验证码
-        return true;
+    public Boolean sendVerifyCode(String phone) {
+        //生成验证码
+        String code = NumberUtils.generateCode(5);
+        try {
+            //发送消息通知 发验证码短信
+            Map<String, String> msg = new HashMap<>();
+            msg.put("phone",phone);
+            msg.put("code",code);
+            amqpTemplate.convertAndSend("ly.sms.exchange","sms.verify.code",msg);
+            //将验证码存入redis,设置失效时间
+            redisTemplate.opsForValue().set(CODE_PRIFIX+phone,code,5, TimeUnit.MINUTES);
+            return true;
+        } catch (AmqpException e) {
+            logger.error("发送短信失败,phone：{}， code：{}", phone, code);
+            e.printStackTrace();
+        }
+        return false;
     }
 }
 
